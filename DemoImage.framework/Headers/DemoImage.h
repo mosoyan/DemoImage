1f82277a5b84cfc2843b910e69a4ad0d99982857
Hovsep Keropyan <hovsep.keropyan@varmtech.com>
2021-01-22 19:50:15 +0400

    init

---
+++ DemoImage.framework/Headers/DemoImage.h
@@ -0,0 +1,18 @@
+//
+//  DemoImage.h
+//  DemoImage
+//
+//  Created by Hovsep Keropyan on 1/22/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DemoImage.
+FOUNDATION_EXPORT double DemoImageVersionNumber;
+
+//! Project version string for DemoImage.
+FOUNDATION_EXPORT const unsigned char DemoImageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DemoImage/PublicHeader.h>
+
+
